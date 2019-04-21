078c0ef8b440497692ca85ca135b96a54aec5733
maskleo.com <maskleo@qq.com>
2019-04-21 22:16:58 +0800

    add

---
+++ collection/src/com/maskleo/basic/collection/Test01.java
@@ -0,0 +1,19 @@
+package com.maskleo.basic.collection;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        try {
+           Method method = ArrayList.class.getMethod("add",Object.class);
+           method.invoke(list,"a");
+            System.out.println(list);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
